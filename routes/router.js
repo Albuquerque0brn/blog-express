c775434859cf46977232ccfc1280344fead8287f
Albuquerque <albuquerque.basa@gmail.com>
2025-04-20 11:29:57 -0300

    first commit

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Página inicial');
-});
+const postsController = require('../controllers/postsController');
+const adminController = require('../controllers/adminController');
+
+router.get('/', postsController.index);
+router.get('/posts/:id', postsController.show);
+
+router.get('/admin', adminController.index);
+router.get('/admin/create', adminController.create);
+router.post('/admin/create', express.urlencoded({ extended: true }), adminController.save);
+router.get('/admin/edit/:id', adminController.edit);
+router.post('/admin/update/:id', express.urlencoded({ extended: true }), adminController.update);
+router.post('/admin/delete/:id', express.urlencoded({ extended: true }), adminController.delete);
 
 module.exports = router;
